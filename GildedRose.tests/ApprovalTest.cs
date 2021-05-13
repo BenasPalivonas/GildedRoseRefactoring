7f0eb34e7598c368054c4011a6371ce2c480eca0
BenasPalivonas <palivonas.benas@gmail.com>
2021-05-14 00:12:13 +0300

    creating a seperate project for unit tests

--- GildedRose.tests/ApprovalTest.cs
+++ GildedRose.tests/ApprovalTest.cs
@@ -4,8 +4,9 @@ using System.IO;
 using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using csharpcore;
 
-namespace csharpcore
+namespace GildedRose.tests
 {
     [UseReporter(typeof(DiffReporter))]
     public class ApprovalTest
